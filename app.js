71ae0702e4f945d2c50bd6b5694a6d19733f1c61
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-07-10 20:24:18 +0800

    app.js file aa uusgej supabase te holbolt hiij ogov

---
+++ app.js
@@ -0,0 +1,5 @@
+// supabase holbolt
+const SUPABASE_URL = "https://mgkknnhbsmkhlhahudzq.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_ijQMQQfZB3jAlKq4KNQu5g_lpiwhHsf";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
